affc12b98d388bbbbb5944440a1bb485c6e18d59
lilstar <2762094853@qq.com>
2022-10-03 19:28:21 +0800

    First commit

---
+++ src/test/java/org/bcx/tut4/TestCalc.java
@@ -0,0 +1,28 @@
+package org.bcx.tut4;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestCalc {
+    Calc cal;
+
+    @BeforeEach
+    void setUp(){
+        cal = new Calc();
+    }
+
+    @AfterEach
+    void tearDown(){
+        cal = null;
+    }
+
+    @Test
+    void add() {
+        assertEquals(30, Calc.add(10, 20));
+    }
+
+
+}
